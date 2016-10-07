9e7a26a8f10083d3d1206fc6b4cde05f3298c581
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2016-10-07 20:55:31 +0200

    Initial commit

---
+++ src/main/scala/sangria/streaming/ValidOutStreamType.scala
@@ -0,0 +1,18 @@
+package sangria.streaming
+
+import scala.annotation.implicitNotFound
+
+@implicitNotFound(msg = "${Res} is invalid type for the resulting GraphQL type ${Out}.")
+trait ValidOutStreamType[-Res, +Out]
+
+object ValidOutStreamType extends LowPrioValidOutType {
+  implicit def validSubclass[Res, Out](implicit ev: Res <:< Out) = valid.asInstanceOf[ValidOutStreamType[Res, Out]]
+  implicit def validNothing[Out] = valid.asInstanceOf[ValidOutStreamType[Nothing, Out]]
+  implicit def validOption[Res, Out](implicit ev: Res <:< Out) = valid.asInstanceOf[ValidOutStreamType[Res, Option[Out]]]
+}
+
+trait LowPrioValidOutType {
+  val valid = new ValidOutStreamType[Any, Any] {}
+
+  implicit def validSeq[Res, Out](implicit ev: Res <:< Out) = valid.asInstanceOf[ValidOutStreamType[Res, Seq[Out]]]
+}
